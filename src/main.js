7912182c5ee07bb84ee4fb2d771a5d1ea20d562b
dev-eugen <75083421+dev-eugen@users.noreply.github.com>
2022-10-03 23:40:37 +0300

    2 files modified

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const { createApp } = require('vue');
+import { createApp } from 'vue';
 import BootstrapVue3 from 'bootstrap-vue-3';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
